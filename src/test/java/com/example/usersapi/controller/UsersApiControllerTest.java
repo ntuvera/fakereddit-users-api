95e19f7730783714d0ec9ede6165249348672117
Steven Huang <steven.charles.huang@gmail.com>
2019-11-30 12:37:47 -0500

    added listAll test

--- src/test/java/com/example/usersapi/controller/UsersApiControllerTest.java
+++ src/test/java/com/example/usersapi/controller/UsersApiControllerTest.java
@@ -7,7 +7,8 @@ import static org.mockito.ArgumentMatchers.anyString;
 
 import com.example.usersapi.model.JwtResponse;
 import com.example.usersapi.model.User;
-import com.example.usersapi.service.UserService;
+import com.example.usersapi.model.UserProfile;
+import com.example.usersapi.model.UserRole;
 import com.example.usersapi.service.UserServiceImpl;
 import com.example.usersapi.util.JwtUtil;
 import org.junit.Before;
@@ -21,11 +22,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -45,7 +50,7 @@ public class UsersApiControllerTest {
     private UserServiceImpl userService;
 
     @MockBean
-    JwtUtil jwtUtil;
+    private JwtUtil jwtUtil;
 
     @InjectMocks
     private User user;
@@ -53,16 +58,38 @@ public class UsersApiControllerTest {
     @InjectMocks
     private JwtResponse jwtResponse;
 
+    @InjectMocks
+    private UserRole userRole;
+
+    @InjectMocks
+    private UserProfile userProfile;
+
+    private List<User> userList;
+
     @Before
     public void init() {
         jwtResponse.setToken("12345");
         jwtResponse.setId(1);
         jwtResponse.setUsername("testUser");
 
+        userRole.setId(3);
+        userRole.setName("ROLE_USER");
+
+        userProfile.setId(1);
+        userProfile.setUserId(1);
+        userProfile.setAdditionalEmail("additionalEmail@email.com");
+        userProfile.setAddress("123 Test St.");
+        userProfile.setMobile("1-800-TEST-NUM");
+
         user.setId(1);
         user.setUsername("testUser");
         user.setPassword("testPass");
         user.setEmail("user@testmail.com");
+        user.setUserRole(userRole);
+        user.setUserProfile(userProfile);
+
+        userList = new ArrayList<>();
+        userList.add(user);
     }
 
     @Test
@@ -70,7 +97,7 @@ public class UsersApiControllerTest {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .post("/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\":\"testUser\",\"email\":\"user@testmail.com\",\"password\":\"testPass\",\"userRole\":{\"name\": \"ROLE_ADMIN\"}}");
+                .content("{\"username\":\"testUser\",\"email\":\"user@testmail.com\",\"password\":\"testPass\",\"userRole\":{\"name\": \"ROLE_USER\"}}");
 
         when(userService.signUpUser(any())).thenReturn(jwtResponse);
 
@@ -99,6 +126,26 @@ public class UsersApiControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"token\":\"12345\",\"username\":\"testUser\",\"id\":1}"))
                 .andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", password = "admin", roles = {"ADMIN"})
+    public void listAll_UserList_Success() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/list")
+                .header("Authorization", "");
+
+        when(userService.listAll()).thenReturn(userList);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk());
+
+        MvcResult result = mockMvc
+                .perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":1,\"username\":\"testUser\",\"password\":\"testPass\",\"email\":\"user@testmail.com\",\"userProfile\":{\"id\":1,\"additionalEmail\":\"additionalEmail@email.com\",\"mobile\":\"1-800-TEST-NUM\",\"address\":\"123 Test St.\",\"userId\":1},\"userRole\":{\"id\":3,\"name\":\"ROLE_USER\"}}]"))
+                .andReturn();
+
         System.out.println(result.getResponse().getContentAsString());
     }
 
